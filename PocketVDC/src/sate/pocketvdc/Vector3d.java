26ce59306b7fb518fd4b59514b79c574f796e59d
James West <christmascow009@gmail.com>
2012-07-06 17:25:50 -0400

    Added comment for vector3d constructor

--- PocketVDC/src/sate/pocketvdc/Vector3d.java
+++ PocketVDC/src/sate/pocketvdc/Vector3d.java
@@ -12,6 +12,12 @@ public class Vector3d
 		x = y = z = 0.0f;
 	}
 	
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @param z
+	 */
 	public Vector3d(float x, float y, float z){
 		this.x = x;
 		this.y = y;
